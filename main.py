bd2bfd5a2be6c91a0b0104e9ef87be0b32b243fa
alexander <2107459b@student.gla.ac.uk>
2017-10-15 20:00:29 +0100

    Now we can take the whole data and place it to our database

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tweepy
 from pymongo import MongoClient
 import re
+import json
 
 auth = tweepy.OAuthHandler('q4utaFepGhE5OjujyoruBOoQg', 'D5K3P5URNUTxKnoVnggiUFsNapuNLOSx5cB7Zh6Y4HhpBhhtNy')
 auth.set_access_token('438291047-AWXl0LpNxZzjhdFA3FH7AJHtmLRK52QDJiKzq5Wz', 'o3kZKFF2s9ctgVpfDVRRpMbg6BMsGUIFWlJm9wSysKyyY')
@@ -12,13 +13,17 @@ db = client.twitterdb
 
 class MyStreamListener(tweepy.StreamListener):
 
-    def on_status(self, status):
-        print(status.text)
-        insert = {
-            "Title" : status.text
-        }
-        post_id = db.col.insert_one(insert).inserted_id
+    def on_data(self, raw_data):
+        json_data = json.loads(raw_data)
+        post_id = db.twitter_data.insert_one(json_data).inserted_id
         print(post_id)
+    #def on_status(self, status):
+        #print(status.text)
+        #insert = {
+        #    "Title" : status.text
+        #}
+        #post_id = db.col.insert_one(insert).inserted_id
+        #print(post_id)
     def on_error(self, status_code):
         if status_code == 420:
             # returning False in on_data disconnects the stream
@@ -26,12 +31,12 @@ class MyStreamListener(tweepy.StreamListener):
 
 myStream = tweepy.Stream(auth = api.auth, listener=MyStreamListener())
 
-#myStream.filter(locations=[-4.50,55.79,-3.97,55.93], async=True)
+myStream.filter(locations=[-4.50,55.79,-3.97,55.93], async=True)
 
-print(db.col.count())
+#print(db.col.count())
 filter = "Scotland"
 regx = re.compile(".*"+filter+".*", re.IGNORECASE)
 posts = db.col.find({"Title": regx})
 for post in posts:
     print(post)
-print(posts.count())
+#print(posts.count())
