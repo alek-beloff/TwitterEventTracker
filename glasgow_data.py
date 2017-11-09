4a9543247830f0a56ac6c65e44f620884cd530b6
tcchanaf <tcchanaf@ust.hk>
2017-11-09 16:42:57 +0000

    Updated version of ipywidgets and added historical data retrieval

--- glasgow_data.py
+++ glasgow_data.py
@@ -15,10 +15,11 @@ auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 
-client = MongoClient()
-db = client.twitterdb
+ client = MongoClient()
+ db = client.twitterdb
 def search_tweets():
     for tweet in tweepy.Cursor(api.search,geocode="55.86515,-4.25763,10km",lang= "en",include_entities=True).items():
+        #no geo info
         if(tweet.user.geo_enabled == False):
 
             print(tweet._json)
@@ -27,18 +28,70 @@ def search_tweets():
             }
             db.hist_glasgow_nogeolocation.insert_one(insert).inserted_id
 
+        #exact coord
         if (tweet.user.geo_enabled == True and tweet.coordinates != None):
             print(tweet._json)
             insert = {
                 tweet._json
             }
             db.hist_glasgow_geo_coordinates.insert_one(insert).inserted_id
-       # if (tweet.user.geo_enabled == True and tweet.bounding_box != None and tweet.coordinates == None):
-        #    print(tweet._json)
-         #   insert = {
-          #      tweet._json
-           # }
-           # db.hist_glasgow_bounding_box.insert_one(insert).inserted_id
+
+        ##bounding box
+        if (tweet.user.geo_enabled == True and tweet.place != None and tweet.coordinates == None):
+            # print(decodeData(tweet))
+            print(tweet.place.bounding_box.coordinates)
+            insert = {
+                tweet._json
+            }
+            db.hist_glasgow_bounding_box.insert_one(insert).inserted_id
+
+
+
+def decodeData(status):
+    return {
+        "retweet_count": status.retweet_count,
+        "favorited": status.favorited,
+        "in_reply_to_user_id": status.in_reply_to_user_id,
+        "created_at": status.created_at,
+        "coordinates": status.coordinates,
+        "user": {
+            "created_at": status.user.created_at,
+            "geo_enabled": status.user.geo_enabled,
+            "lang": status.user.lang,
+            "url": status.user.url,
+            "description": status.user.description,
+            "time_zone": status.user.time_zone,
+            "location": status.user.location,
+            "screen_name": status.user.screen_name,
+            "protected": status.user.protected,
+            "statuses_count": status.user.statuses_count,
+            "profile_image_url_https": status.user.profile_image_url_https,
+            "utc_offset": status.user.utc_offset,
+            "followers_count": status.user.followers_count,
+            "id": status.user.id,
+            "id_str": status.user.id_str,
+            "name": status.user.name,
+            "friends_count": status.user.friends_count,
+        },
+        "retweeted": status.retweeted,
+        "place": {
+            "country_code": status.place.country_code,
+            "country": status.place.country,
+            "name": status.place.name,
+            "full_name": status.place.full_name,
+            "id": status.place.id,
+            "bounding_box": {
+                "type": status.place.bounding_box.type,
+                "coordinates": status.place.bounding_box.coordinates
+            }
+        },
+        "geo": status.geo,
+        "_id": status.id,
+        "text": status.text,
+        "lang": status.lang,
+        "in_reply_to_user_id_str": status.in_reply_to_user_id_str,
+        "id_str": status.id_str
+    }
 
 search_tweets()
 
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 from pymongo import MongoClient
 import re
 import json
+import math
+import folium
 
 auth = tweepy.OAuthHandler('q4utaFepGhE5OjujyoruBOoQg', 'D5K3P5URNUTxKnoVnggiUFsNapuNLOSx5cB7Zh6Y4HhpBhhtNy')
 auth.set_access_token('438291047-AWXl0LpNxZzjhdFA3FH7AJHtmLRK52QDJiKzq5Wz', 'o3kZKFF2s9ctgVpfDVRRpMbg6BMsGUIFWlJm9wSysKyyY')
@@ -45,3 +47,32 @@ posts = db.twitter_data.find({"text": regx})
 for post in posts:
     print(post["place"]["bounding_box"]["type"])
 print(posts.count())
+
+
+def startMap(sender):
+    m = folium.Map(location=[(55.79+55.93)/2, (-4.50-3.97)/2])
+    filter = "is"
+    regx = re.compile(".*"+filter+".*", re.IGNORECASE)
+    posts = db.twitter_data.find({"text": regx})
+    for post in posts:
+            x0 = post["place"]["bounding_box"]["coordinates"][0][0][0]
+            x1 = post["place"]["bounding_box"]["coordinates"][0][1][0]
+            x2 = post["place"]["bounding_box"]["coordinates"][0][2][0]
+            y0 = post["place"]["bounding_box"]["coordinates"][0][0][1]
+            y1 = post["place"]["bounding_box"]["coordinates"][0][1][1]
+            y2 = post["place"]["bounding_box"]["coordinates"][0][2][1]
+            sq = np.square([x1-x0, y1-y0, x2-x1, y2-y1])
+            sqrts = np.sqrt([sq[0]+sq[1], sq[2]+sq[3]])
+            L1 = sqrts[0]
+            L2 = sqrts[1]
+            R = math.sqrt(L1**2 + L2**2)
+            centre=[(y2+y0)/2, (x2+x0)/2]
+            text = ''.join(e for e in post["text"] if e.isalnum() or e==' ')[:40]
+            text = '<i>' + text + '</i>'
+            if (R>1.0):
+                folium.CircleMarker(centre, radius=R, popup=text,
+                                color='#3186cc', fill_color='#3186cc').add_to(m)
+            else:
+                folium.Marker(centre, popup=text,
+                             icon=folium.Icon(color='green',icon='info-sign')).add_to(m)
+    display(m)
