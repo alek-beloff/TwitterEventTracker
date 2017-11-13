764051a06ab84db69f3688b0827bd57afa29710d
alexander <2107459b@student.gla.ac.uk>
2017-11-13 18:22:10 +0000

    new .sh files in data_manage folder

---
+++ data_manage/export_exact.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mongoexport --db twitterdb --collection hist_glasgow_geo_coordinates --out exact.json
---
+++ data_manage/export_nogeo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mongoexport --db twitterdb --collection hist_glasgow_nogeolocation --out nogeo.json
