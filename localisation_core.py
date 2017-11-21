80bcb89869768d9f5101277d964e66c6be6108d4
alexander <2107459b@student.gla.ac.uk>
2017-11-21 03:17:00 +0000

    geo localisation

--- localisation_core.py
+++ localisation_core.py
@@ -7,12 +7,13 @@ from dateutil import parser
 import re
 import json
 from nltk.corpus import stopwords
+from lshash import lshash
 
 def RemoveStopWords(stopWords, text):
     return [w for w in text if w not in stopWords and len(w)>2]
 
 class Tweet:
-    def __init__(self, id, text, time, stopWords, bounding_box = None, coordinates = None):
+    def __init__(self, id, text, time, stopWords, bounding_box = None, coordinates = None, place = None):
         self.id = id
         self.text = RemoveStopWords(stopWords,
                                     list(filter(None,re.split('[^a-z]',text.lower()))))
@@ -22,6 +23,7 @@ class Tweet:
         else:
             self.bounding_box = bounding_box
         self.coordinates = coordinates
+        self.place = place
 
 def getUnloc():
     stopWords = set(stopwords.words('english'))
@@ -45,9 +47,27 @@ def getBbox():
                          json.loads(line)["text"],
                          json.loads(line)["created_at"],
                          stopWords,
-                         bounding_box=json.loads(line)["place"]["bounding_box"]["coordinates"][0])
+                         bounding_box=json.loads(line)["place"]["bounding_box"]["coordinates"][0],
+                         place=json.loads(line)["place"]["name"])
                    for line
                    in tqdm(open("actual_data/bbox.json"))
+                   if json.loads(line)["lang"] == "en"
+                   and json.loads(line)["in_reply_to_user_id"] == None
+                   and json.loads(line)["in_reply_to_status_id"] == None
+                   and json.loads(line)["retweeted"] == False]
+def getGeo():
+    stopWords = set(stopwords.words('english'))
+
+    print('Read geo tweets')
+    return [Tweet(json.loads(line)["_id"]["$numberLong"],
+                         json.loads(line)["text"],
+                         json.loads(line)["created_at"],
+                         stopWords,
+                         bounding_box=json.loads(line)["place"]["bounding_box"]["coordinates"][0],
+                         coordinates=json.loads(line)["coordinates"]["coordinates"],
+                         place=json.loads(line)["place"]["name"])
+                   for line
+                   in tqdm(open("actual_data/exact.json"))
                    if json.loads(line)["lang"] == "en"
                    and json.loads(line)["in_reply_to_user_id"] == None
                    and json.loads(line)["in_reply_to_status_id"] == None
@@ -58,20 +78,59 @@ def qualityTesting(bbox_values, number, threshold, conjunction_matrix, d):
     amount_of_correct = 0
     print("Quality test is running currently...")
     for test in tqdm(bbox_values[:int(number)]):
+        old_bbox = test.bounding_box
         result = localise_to_bbox([test], [x for x in bbox_values if x != test], threshold, conjunction_matrix, d)
         if (len(result) > 0):
             amount_of_localized+=1
-            if list(test.bounding_box) == list(result[0].bounding_box):
+            if list(old_bbox) == list(result[0].bounding_box):
                 amount_of_correct+=1
     l = amount_of_localized*100.0/float(number)
     c = amount_of_correct*100.0/amount_of_localized
     print("Amount of localized tweets is %f percent of the number given"%l)
     print("Amount of correctly localized tweets is %f percent of those which are localized"%c)
 
+def distanceKm(p1,p2):
+    from math import sin, cos, sqrt, atan2, radians
+
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat1 = radians(p1[0])
+    lon1 = radians(p1[1])
+    lat2 = radians(p2[0])
+    lon2 = radians(p2[1])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return R * c
+
+def geoTesting(exact_values, number, threshold, conjunction_matrix, d):
+    amount_of_localized = 0
+    amount_of_correct = 0
+    print("Quality test is running currently...")
+    for test in tqdm(exact_values[:int(number)]):
+        a = [x for x in exact_values if x != test]
+
+        old_coord = test.coordinates
+
+        result = localise_to_geo([test], a, threshold, conjunction_matrix, d)
+        if (len(result) > 0):
+            amount_of_localized+=1
+            dist = distanceKm (old_coord, result[0].coordinates)
+            if dist < 0.1:
+                amount_of_correct+=1
+    l = amount_of_localized*100.0/float(number)
+    c = amount_of_correct*100.0/amount_of_localized
+    print("Amount of localized tweets is %f percent of the number given"%l)
+    print("Amount of correctly localized tweets (within 100 meters) is %f percent of those which are localized"%c)
+
 def localise_to_bbox(unloc, loc, threshold, conj_m, d):
 
     new_col = []
-    from lshash import lshash
 
     lsh = lshash.LSHash(6, conj_m.shape[1])
 
@@ -98,14 +157,62 @@ def localise_to_bbox(unloc, loc, threshold, conj_m, d):
 
             points += [(x, tdelta + 0.0001, idx[1] + 0.0001)
                        for x in bbox.bounding_box]
-            boxes.append(Polygon(bbox.bounding_box))
+            boxes.append(bbox.bounding_box)
         x0 = np.sum([x[0][0] * (1 / x[1] + 1 / x[2]) for x in points])
         y0 = np.sum([x[0][1] * (1 / x[1] + 1 / x[2]) for x in points])
         m0 = np.sum([1 / x[1] + 1 / x[2] for x in points])
         coord_res = Point([x0 / m0, y0 / m0])
         for box in boxes:
-            if box.contains(coord_res):
-                tweet.bounding_box = box.exterior.coords
+            pol = Polygon(box)
+            if pol.contains(coord_res):
+                tweet.bounding_box = box
                 new_col.append(tweet)
                 break
     return new_col
+
+def localise_to_geo(bbox_values,exact_values,threshold,conj_m,d):
+
+    places_dict = {exact.place: [] for exact in exact_values + bbox_values}
+
+    result = []
+
+    for value in exact_values:
+        places_dict[value.place].append(value)
+
+    lsh = {}
+    coord_dict = {}
+
+    for key in places_dict:
+        lsh[key] = lshash.LSHash(6, conj_m.shape[1])
+
+    for place in places_dict:
+        for tweet in places_dict[place]:
+            lsh[place].index(conj_m[d[tweet.id]], extra_data=tweet.id)
+            coord_dict[tweet.id] = tweet
+
+    for bbox in bbox_values:
+        inside_tweets = places_dict[bbox.place]
+        if len(inside_tweets) < 3: continue
+        cs = lsh[bbox.place].query(conj_m[d[bbox.id]], num_results=10, distance_func='cosine')
+
+        cs2 = []
+        for m in cs:
+            if m[1] < threshold:
+                cs2.append([m[0][1], m[1]])
+
+        points = []
+        for idx in cs2:
+            exact = coord_dict[idx[0]]
+
+            tdelta = (exact.time - bbox.time).total_seconds() / timedelta(minutes=1).total_seconds()
+            # another threshold by time. Not more than a week
+            if tdelta > 60 * 24 * 7: continue
+
+            points.append((exact.coordinates, tdelta + 0.0001, idx[1] + 0.0001))
+        if len(points) < 3: continue
+        x0 = np.sum([x[0][0] * (1 / x[1] + 1 / x[2]) for x in points])
+        y0 = np.sum([x[0][1] * (1 / x[1] + 1 / x[2]) for x in points])
+        m0 = np.sum([1 / x[1] + 1 / x[2] for x in points])
+        bbox.coordinates = [x0 / m0, y0 / m0]
+        result.append(bbox)
+    return result
--- localisation_main.py
+++ localisation_main.py
@@ -1,12 +1,8 @@
 from localisation_core import *
 import sys
 
-if len(sys.argv) > 1 and sys.argv[1] != 'testBB':
+if len(sys.argv) > 1 and sys.argv[1].lower() not in ['testbb', 'testgeo'] :
     exit('argument is incorrect')
-
-if len(sys.argv) > 1 and sys.argv[1] == 'testBB':
-    bbox_values = getBbox()
-    print("and number of bbox is %d" % len(bbox_values))
 
 nonloc_values = []
 if len(sys.argv) == 1:
@@ -36,13 +32,38 @@ print("Matrix is calculated. Shape is", conjunction_matrix.shape)
 #make a threshold for similarity
 threshold = 0.9
 
-if len(sys.argv) > 1 and sys.argv[1] == 'testBB':
-    if len(sys.argv) > 2:
-        qualityTesting(bbox_values, sys.argv[2], threshold=threshold, conjunction_matrix=conjunction_matrix, d=d)
-    else:
-        #default parameter
-        qualityTesting(bbox_values, 10, threshold=threshold, conjunction_matrix=conjunction_matrix, d=d)
+if len(sys.argv) > 2 and sys.argv[1].lower() == 'testbb':
+    qualityTesting(bbox_values, sys.argv[2], threshold=threshold, conjunction_matrix=conjunction_matrix, d=d)
+    exit('testing is completed')
 
+exact_values = getGeo()
+
+new_col = []
 if len(sys.argv) == 1:
-    new_col = localise_to_bbox(nonloc_values, bbox_values, threshold, conjunction_matrix, d)
+    new_col = localise_to_bbox(nonloc_values[:100], bbox_values, threshold, conjunction_matrix, d)
     print("We could recognize %f per cent tweets" % (len(new_col) * 100.0 / len(nonloc_values)))
+
+#join recognised values to bbox
+bbox_values += new_col
+
+exact = [value.text for value in exact_values + bbox_values]
+# flatten the list of lists to 1d array
+exact_flatten = [item for sublist in exact for item in sublist]
+# remove duplicates
+exact_dict = {w: '' for w in exact_flatten}
+# enumerate without duplicates
+exact_enum = {w: idx for idx, w in enumerate(exact_dict)}
+
+exact_matrix = np.zeros((len(exact_values + bbox_values), len(exact_enum)), dtype=int)
+d = dict()
+for idx, tweet in enumerate(exact_values + bbox_values):
+    d[tweet.id] = idx
+    for w in tweet.text:
+        exact_matrix[idx, exact_enum[w]] += 1
+
+if len(sys.argv) > 2 and sys.argv[1].lower() == 'testgeo':
+    geoTesting(exact_values, sys.argv[2], threshold=threshold, conjunction_matrix=exact_matrix, d=d)
+    exit('testing is completed')
+
+exacts = localise_to_geo(bbox_values, exact_values, threshold, exact_matrix, d)
+print(len(exacts))
