1cf90d98f004f9819cd91638d7d670ade06c23bd
alexander <2107459b@student.gla.ac.uk>
2017-11-28 01:00:12 +0000

    streaming

--- streaming_data.py
+++ streaming_data.py
@@ -4,10 +4,10 @@ import time
 import sys
 
 
-CONSUMER_KEY = 'cwuOhOSiMHaqSjUsyfYRVltuE', 'q4utaFepGhE5OjujyoruBOoQg'
-CONSUMER_SECRET = 'JBZWaPi3ldDHgMo6NPr8MbRKEU2iHBW7xVzL094HjsoX33K4eJ', 'D5K3P5URNUTxKnoVnggiUFsNapuNLOSx5cB7Zh6Y4HhpBhhtNy'
-OAUTH_TOKEN = '842632842207203328-cNbwTaG4eW4rbQJwaG4RxtZkHJ51SoO', '438291047-AWXl0LpNxZzjhdFA3FH7AJHtmLRK52QDJiKzq5Wz'
-OAUTH_TOKEN_SECRET = 'IhypdlKWPYtpKJ8aWevWTPTyeTbtmffVRGsFcF9hXkQQg', 'o3kZKFF2s9ctgVpfDVRRpMbg6BMsGUIFWlJm9wSysKyyY'
+CONSUMER_KEY = 'cwuOhOSiMHaqSjUsyfYRVltuE', 'q4utaFepGhE5OjujyoruBOoQg', '8ZHXLlxEfLQm90U19fRC08NBx', 'r8dh3IsvxNOMH2UjSIoM00gnN'
+CONSUMER_SECRET = 'JBZWaPi3ldDHgMo6NPr8MbRKEU2iHBW7xVzL094HjsoX33K4eJ', 'D5K3P5URNUTxKnoVnggiUFsNapuNLOSx5cB7Zh6Y4HhpBhhtNy','OjxPXXR4lDc666H2HUWtbQBtG2J5d2wDZY1B6XirQHpyZqGEbY', '0SqGVySPhZ8ngMYnQ05W8KPctMp8jbXGSdCo0qKxMYKwcewIrZ'
+OAUTH_TOKEN = '842632842207203328-cNbwTaG4eW4rbQJwaG4RxtZkHJ51SoO', '438291047-AWXl0LpNxZzjhdFA3FH7AJHtmLRK52QDJiKzq5Wz','916331671372353536-lVwpfVwieRCuLmyP14j0lbXGuNcitcD', '917416602634768385-pXPkTeyW9vaysd4vZflYm2pZckkIeDn'
+OAUTH_TOKEN_SECRET = 'IhypdlKWPYtpKJ8aWevWTPTyeTbtmffVRGsFcF9hXkQQg', 'o3kZKFF2s9ctgVpfDVRRpMbg6BMsGUIFWlJm9wSysKyyY', 'gdpRf9Qf2cU01yGPem2aJaP6sljaEah1lDdPRtyt2b75b', 'HweGKohJFWSMPDj1LwjoNExGIj1K2e7ApHdHpA7fcwl7F'
 
 idx = 0
 if len(sys.argv) == 1:
@@ -31,38 +31,93 @@ db = client.twitterdb
 
 
 class MyStreamListener(tweepy.StreamListener):
-    def on_status(self, status):
-
-        #if (status.place != None and status.place.name in ['NY']):# and status.place.name in ['Glasgow', 'New York', 'Chicago', 'London']):
-         #   print("PLACE IS NOT NULL", status.coordinates, status.place.bounding_box.coordinates)
-        #print(status)
-        #if (status.user.geo_enabled == False):print ("it is me!!")
-
-        ##bounding box data
-
-        locations = ['Glasgow', 'Manhattan', 'Staten Island', 'Queens', 'Floral Park', 'Yaphank', 'Brooklyn',
-                     'Dobbs Ferry', 'North Massapequa', 'Bronx']
-
+    def checkLO(status):
+        locations = ['Royal Albert Hall', 'Brent', 'Lambeth', 'London', 'Islington', 'Kensington', 'Enfield', 'Eltham', 
+            'Walthamstow', 'Merton', 'Paddington', 'Camden Town', 'Wandsworth', 'Crayford', 'Hackney', 'Richmond', 'Greenwich', 'Barnet',
+            'East Ham', 'West Ham', 'Hillingdon', 'Staines-upon-Thames', 'Hammersmith', 'South East', 'Water Rats', 'Poplar', 'Sutton', 'Stratford',
+            'Wembley Stadium', 'Hackney', 'Camberwell', 'Bexley', 'Ealing', 'Romford', 'Kingston upon Thames', 'East', 'Harrow', 'The O2']
         if (status.coordinates == None and status.place!=None):
             if (status.place.name not in locations): return True
             print(status.place.name, status.place.bounding_box.coordinates[0][::2])
-            #print (status.place.name)
             a = status._json
             a["_id"] = a["id"]
             try:
-                db.stream_boundingBox.insert_one(a).inserted_id
+                db.stream_boundingBoxLO.insert_one(a).inserted_id
             except:
                 print("duplicated!")
         elif (status.coordinates != None):
             if (status.place.name not in locations): return True
-            #print('coordinates', status._json)
             a = status._json
             a["_id"] = a["id"]
             try:
-                db.stream_geo_coordinates.insert_one(a).inserted_id
+                db.stream_geo_coordinatesLO.insert_one(a).inserted_id
             except:
                 print("duplicated!")
-        else:
+
+    def checkNY(status):
+        locations = ['Manhattan', 'Staten Island', 'Queens', 'Floral Park', 'Yaphank', 'Brooklyn',
+                     'Dobbs Ferry', 'North Massapequa', 'Bronx', 'New York']
+        if (status.coordinates == None and status.place!=None):
+            if (status.place.name not in locations): return True
+            print(status.place.name, status.place.bounding_box.coordinates[0][::2])
+            a = status._json
+            a["_id"] = a["id"]
+            try:
+                db.stream_boundingBoxNY.insert_one(a).inserted_id
+            except:
+                print("duplicated!")
+        elif (status.coordinates != None):
+            if (status.place.name not in locations): return True
+            a = status._json
+            a["_id"] = a["id"]
+            try:
+                db.stream_geo_coordinatesNY.insert_one(a).inserted_id
+            except:
+                print("duplicated!")
+
+    def checkGLA(status):
+        locations = ['Glasgow']
+        if (status.coordinates == None and status.place!=None):
+            if (status.place.name not in locations): return True
+            print(status.place.name, status.place.bounding_box.coordinates[0][::2])
+            a = status._json
+            a["_id"] = a["id"]
+            try:
+                db.stream_boundingBoxGLA.insert_one(a).inserted_id
+            except:
+                print("duplicated!")
+        elif (status.coordinates != None):
+            if (status.place.name not in locations): return True
+            a = status._json
+            a["_id"] = a["id"]
+            try:
+                db.stream_geo_coordinatesGLA.insert_one(a).inserted_id
+            except:
+                print("duplicated!")
+
+    def checkCHI(status):
+        locations = ['Chicago', 'Illinois', 'Indiana', 'Michigan']
+        if (status.coordinates == None and status.place!=None):
+            if (status.place.name not in locations): return True
+            print(status.place.name, status.place.bounding_box.coordinates[0][::2])
+            a = status._json
+            a["_id"] = a["id"]
+            try:
+                db.stream_boundingBoxCHI.insert_one(a).inserted_id
+            except:
+                print("duplicated!")
+        elif (status.coordinates != None):
+            if (status.place.name not in locations): return True
+            a = status._json
+            a["_id"] = a["id"]
+            try:
+                db.stream_geo_coordinatesCHI.insert_one(a).inserted_id
+            except:
+                print("duplicated!")
+
+    def on_status(self, status):
+
+        if (status.coordinates == None and status.place == None):
             a = status._json
             a["_id"] = a["id"]
             try:
@@ -70,6 +125,13 @@ class MyStreamListener(tweepy.StreamListener):
                 db.stream_nongeo_coordinates.insert_one(a).inserted_id
             except:
                 print("duplicated!")
+        else:
+            if sys.argv[1] == 'loc' and sys.argv[2].lower() == 'ny':
+                checkNY(status)
+            if sys.argv[1] == 'loc' and sys.argv[2].lower() == 'lo':
+                checkLO(status)
+            if sys.argv[1] == 'loc' and sys.argv[2].lower() == 'gla':
+                checkGLA(status)
         return True
 
     def on_error(self, status_code):
@@ -80,13 +142,6 @@ class MyStreamListener(tweepy.StreamListener):
 
 
 myStream = tweepy.Stream(auth=api.auth, listener=MyStreamListener())
-
-##filter for tweets from NYC area
-# myStream.filter(locations=[80.10,12.90,80.33,13.24], async=True)
-
-# myStream.filter(track=['syria%paris'])
-#myStream.filter(locations=[-89.99,-89.99,89.99,89.99], async=True)
-# myStream.sample()
 
 def Sample (myStream, lang):
     t = 0
@@ -105,7 +160,8 @@ def Sample (myStream, lang):
             time.sleep(60 * 15)
             t += 1
 
-def Filter (myStream):
+
+def FilterLO (myStream):
     t = 0
     while (True):
         print("running filter", t)
@@ -114,8 +170,23 @@ def Filter (myStream):
         if t >= 20:
             break
         try:
-            myStream.filter(locations=[-4.393285, 55.796184, -4.090218, 55.920421,  # Glasgow
-                                       -74.026675, 40.683935, -73.910408, 40.877483,  # Manhattan
+            myStream.filter(locations=[-0.510365, 51.286702, 0.334043, 51.691824]) #Greater London
+        except:
+            print(t)
+            ## sleep for 15mins if error
+            time.sleep(60 * 15)
+            t += 1
+
+def FilterNY (myStream):
+    t = 0
+    while (True):
+        print("running filter", t)
+
+        ##run the program for 20 times
+        if t >= 20:
+            break
+        try:
+            myStream.filter(locations=[-74.026675, 40.683935, -73.910408, 40.877483,  # Manhattan
                                        -74.255641, 40.495865, -74.052253, 40.648887,  # Staten Island
                                        -73.962582, 40.541722, -73.699793, 40.800037,  # Queens
                                        -73.722827, 40.712833, -73.687894, 40.73732,  # Floral Park (NY)
@@ -124,19 +195,54 @@ def Filter (myStream):
                                        -73.883553, 40.998332, -73.845404, 41.02466,  # Dobbs Ferry (NY)
                                        -73.486593, 40.681111, -73.450056, 40.718254,  # North Massapequa
                                        -73.933612, 40.785365, -73.765271, 40.91533,  # Bronx
-                                       ])
+                                       ]) 
         except:
             print(t)
             ## sleep for 15mins if error
             time.sleep(60 * 15)
             t += 1
 
+def FilterGLA (myStream):
+    t = 0
+    while (True):
+        print("running filter", t)
+
+        ##run the program for 20 times
+        if t >= 20:
+            break
+        try:
+            myStream.filter(locations=[-4.393285, 55.796184, -4.090218, 55.920421]) #Glasgow
+        except:
+            print(t)
+            ## sleep for 15mins if error
+            time.sleep(60 * 15)
+            t += 1
+
+def FilterCHI (myStream):
+    t = 0
+    while (True):
+        print("running filter", t)
+
+        ##run the program for 20 times
+        if t >= 20:
+            break
+        try:
+            myStream.filter(locations=[-87.940033, 41.644102, -87.523993, 42.023067]) #Chicago
+        except:
+            print(t)
+            ## sleep for 15mins if error
+            time.sleep(60 * 15)
+            t += 1 
+
 if sys.argv[1] == 'nonloc':
     print("streaming nonloc")
     Sample(myStream,["en"])
 if sys.argv[1] == 'loc':
-    print("streaming loc")
-    Filter(myStream)
-
-
-#myStream.filter(locations=[-74.284596, 40.502686, -72.474598, 41.336975, -4.3932845, 55.796184,-4.0902182,55.9204214], async=True)
+    if sys.argv[1] == 'loc' and sys.argv[2].lower() == 'ny':
+        FilterNY(myStream)
+    if sys.argv[1] == 'loc' and sys.argv[2].lower() == 'lo':
+        FilterLO(myStream)
+    if sys.argv[1] == 'loc' and sys.argv[2].lower() == 'gla':
+        FilterGLA(myStream)
+    if sys.argv[1] == 'loc' and sys.argv[2].lower() == 'chi':
+        FilterCHI(myStream)
