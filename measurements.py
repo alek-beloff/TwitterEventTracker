f660f1976165666e33b981d3a82e422e035e7d61
alexander <2107459b@student.gla.ac.uk>
2017-12-04 05:59:17 +0000

    world distribution

--- measurements.py
+++ measurements.py
@@ -9,8 +9,21 @@ def averageWorldDistribution ():
 	
 	cursor = unloc.find({},{'created_at': 1})
 	tweets = []
+	times = []
 
+	i = 0
+
+	print("getting tweets from database")
 	for tweet in tqdm(cursor):
    		tweets.append(tweet)
+   		time = parser.parse(str(tweet['created_at']))
+   		print (tweet['created_at'], time)
+   		i += 1
+   		if i==10:
+   			return
+
+
+
+
 
 averageWorldDistribution()
