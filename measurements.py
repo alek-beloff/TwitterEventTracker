3b6c3770780591cdfe9d4826141fec015ff6d768
alexander <2107459b@student.gla.ac.uk>
2017-12-04 05:39:37 +0000

    world distribution

---
+++ measurements.py
@@ -0,0 +1,14 @@
+import sys
+from pymongo import MongoClient
+import numpy as np
+import tqdm
+
+def averageWorldDistribution ():
+	client = MongoClient()
+	unloc = client.twitterdb.stream_nongeo_coordinates
+	
+	tweets = tqdm(unloc.find({},{created_at: 1}))
+
+	print(tweets[0])
+
+averageWorldDistribution()
