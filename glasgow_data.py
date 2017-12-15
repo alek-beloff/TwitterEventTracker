00885688c36ddd7284f0482f0084016e1c24432b
tcchanaf <tcchanaf@ust.hk>
2017-12-15 15:45:17 +0000

    FinalPlot updated by ctc

--- glasgow_data.py
+++ glasgow_data.py
@@ -3,42 +3,48 @@ from pymongo import MongoClient
 import time
 # Get a cursor object
 
-CONSUMER_KEY = 'cwuOhOSiMHaqSjUsyfYRVltuE'
-CONSUMER_SECRET = 'JBZWaPi3ldDHgMo6NPr8MbRKEU2iHBW7xVzL094HjsoX33K4eJ'
-OAUTH_TOKEN = '842632842207203328-cNbwTaG4eW4rbQJwaG4RxtZkHJ51SoO'
-OAUTH_TOKEN_SECRET = 'IhypdlKWPYtpKJ8aWevWTPTyeTbtmffVRGsFcF9hXkQQg'
+# CONSUMER_KEY = 'cwuOhOSiMHaqSjUsyfYRVltuE'
+# CONSUMER_SECRET = 'JBZWaPi3ldDHgMo6NPr8MbRKEU2iHBW7xVzL094HjsoX33K4eJ'
+# OAUTH_TOKEN = '842632842207203328-cNbwTaG4eW4rbQJwaG4RxtZkHJ51SoO'
+# OAUTH_TOKEN_SECRET = 'IhypdlKWPYtpKJ8aWevWTPTyeTbtmffVRGsFcF9hXkQQg'
+
+CONSUMER_KEY = 'cwuOhOSiMHaqSjUsyfYRVltuE', 'q4utaFepGhE5OjujyoruBOoQg', '8ZHXLlxEfLQm90U19fRC08NBx', 'r8dh3IsvxNOMH2UjSIoM00gnN', 'y5UTqSuap75HuKye2NPcr7rfI'
+CONSUMER_SECRET = 'JBZWaPi3ldDHgMo6NPr8MbRKEU2iHBW7xVzL094HjsoX33K4eJ', 'D5K3P5URNUTxKnoVnggiUFsNapuNLOSx5cB7Zh6Y4HhpBhhtNy', 'OjxPXXR4lDc666H2HUWtbQBtG2J5d2wDZY1B6XirQHpyZqGEbY', '0SqGVySPhZ8ngMYnQ05W8KPctMp8jbXGSdCo0qKxMYKwcewIrZ', 'blLk28TL9jEye1DH6CKDrlu4liu1kFssRMHglPXELUlzHzuhaP'
+OAUTH_TOKEN = '842632842207203328-cNbwTaG4eW4rbQJwaG4RxtZkHJ51SoO', '438291047-AWXl0LpNxZzjhdFA3FH7AJHtmLRK52QDJiKzq5Wz', '916331671372353536-lVwpfVwieRCuLmyP14j0lbXGuNcitcD', '917416602634768385-pXPkTeyW9vaysd4vZflYm2pZckkIeDn', '917723257217998848-uld992dlGdvz71FpxosLs7gjAUCuIbI'
+OAUTH_TOKEN_SECRET = 'IhypdlKWPYtpKJ8aWevWTPTyeTbtmffVRGsFcF9hXkQQg', 'o3kZKFF2s9ctgVpfDVRRpMbg6BMsGUIFWlJm9wSysKyyY', 'gdpRf9Qf2cU01yGPem2aJaP6sljaEah1lDdPRtyt2b75b', 'HweGKohJFWSMPDj1LwjoNExGIj1K2e7ApHdHpA7fcwl7F', 'bkDjxKNVddeDwBUIJo1mL5ENz3JTMD2Ka2jyJvAyGxsfC'
 
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-api = tweepy.API(auth)
+def changeAPI(id):
+    a = tweepy.OAuthHandler(CONSUMER_KEY[id], CONSUMER_SECRET[id])
+    a.set_access_token(OAUTH_TOKEN[id], OAUTH_TOKEN_SECRET[id])
+    return tweepy.API(a)
 
-
-client = MongoClient()
-db = client.twitterdb
 def search_tweets():
     for tweet in tweepy.Cursor(api.search, geocode="55.86515,-4.25763,10km", lang="en", include_entities=True).items():
+        print(tweet.created_at[8:9])
+        print(tweet.text)
+        continue
         #no geo info
         if(tweet.user.geo_enabled == False):
 
             print(tweet._json)
             a = tweet._json
             a["_id"] = a["id"]
-            try:
-                db.hist_glasgow_nogeolocation.insert_one(a).inserted_id
-            except:
-                print("duplicated!")
+            # try:
+            #     db.hist_glasgow_nogeolocation.insert_one(a).inserted_id
+            # except:
+            #     print("duplicated!")
 
         #exact coord
         if (tweet.user.geo_enabled == True and tweet.coordinates != None):
             print(tweet._json)
             a = tweet._json
             a["_id"] = a["id"]
-            try:
-                db.hist_glasgow_geo_coordinates.insert_one(a).inserted_id
-            except:
-                print("duplicated!")
+            # try:
+            #     db.hist_glasgow_geo_coordinates.insert_one(a).inserted_id
+            # except:
+            #     print("duplicated!")
 
         ##bounding box
         if (tweet.user.geo_enabled == True and tweet.place != None and tweet.coordinates == None):
@@ -46,10 +52,10 @@ def search_tweets():
             print(tweet.place.bounding_box.coordinates)
             a = tweet._json
             a["_id"] = a["id"]
-            try:
-                db.hist_glasgow_bounding_box.insert_one(tweet._json).inserted_id
-            except:
-                print("duplicated!")
+            # try:
+            #     db.hist_glasgow_bounding_box.insert_one(tweet._json).inserted_id
+            # except:
+            #     print("duplicated!")
 
 
 
@@ -99,8 +105,20 @@ def decodeData(status):
         "id_str": status.id_str
     }
 
+auth = tweepy.OAuthHandler(CONSUMER_KEY[0], CONSUMER_SECRET[0])
+auth.set_access_token(OAUTH_TOKEN[0], OAUTH_TOKEN_SECRET[0])
+
+api = changeAPI(0)
+
+
+client = MongoClient()
+db = client.twitterdb
+
+
 global t
 t = 0
+global i
+i = 0
 while(True):
 
     ##run the program for 200 times
@@ -108,10 +126,15 @@ while(True):
         break
 
     try:
+        api = changeAPI(i)
         search_tweets()
     except:
-        print(t)
+        print('time: ', t)
+        print('id: ', i)
+        i = i + 1
+        if i == 4:
         ## sleep for 15mins if error
-        time.sleep(60 * 15)
-        t += 1
+            time.sleep(60 * 15)
+            t += 1
+            i = 0
 
